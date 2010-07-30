84f389961aef8a9eb8fdce874157e44b20145fa0
iign <iign@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-30 03:41:29 +0000

    Stable Version.
    git-svn-id: https://plugins.svn.wordpress.org/undo-publish/tags/0.2@269146 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Undo Publish ===
+Contributors: ign
+Tags: posts
+Donate Link: http://ign.com.uy
+Requires at least: 3.0
+Tested up to: 3.0.1
+Stable tag: 0.2
+
+Undo Publish let's you stop posts from being published for a few seconds after publishing.
+
+== Description ==
+
+Undo Publish let's you stop posts from being published for a few seconds after publishing. Inspired by Gmail's "Undo Send".
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `plugin-name.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Frequently Asked Questions ==
+
+
+== Upgrade Notice ==
+
+
+== Screenshots ==
+
+1. Cancel Publish Link.
+
+== Changelog ==
+
+= 0.1 =
+First commit.
---
+++ undo_publish.php
@@ -0,0 +1,57 @@
+<?php
+/*
+Description: Undo Publish, inspired by Gmail Labs' "Undo Send". Let's you stop posts from being published for a few seconds after hitting Publish button.
+Plugin Name: Undo Publish
+Plugin URI: http://ign.com.uy/2010/wordpress-plugin-undo-publish
+Author: ign
+Author URI: http://ign.com.uy
+Version: 0.2
+*/
+
+
+add_action( "admin_footer", 'undo_publish' );
+define(TIME_LIMIT, 3000);
+
+function undo_publish() {
+	wp_enqueue_script('jquery');
+	global $parent_file;
+	global $post;
+
+	if ( is_admin() && $parent_file == 'edit.php' && $post->post_status != 'publish'):
+
+	?>
+	
+		<div id="undo-publish" style="display:none; position:absolute; top:0; left:50%; padding: 6px; background-color:#FFFFE0;">
+			<a href="#" onclick="undoPublish();" >Cancel Publish</a>
+		</div>
+	
+		<script type="text/javascript">
+		
+			jQuery(document).ready(function(){
+				jQuery("#publish").hide();
+				jQuery("#publishing-action").append('<input type="submit" value="Publish"  id="publish-alt" class="button-primary" name="publish-alt" />');
+				
+				jQuery('#publish-alt').click(function() {
+					
+					undo_publish = setTimeout("jQuery('#publish').click()", <?php echo TIME_LIMIT; ?>);
+					jQuery('#undo-publish').show();
+					jQuery('#ajax-loading').show();	
+					return false;
+				});
+				
+			});
+
+			function undoPublish(){
+				clearTimeout(undo_publish);
+				jQuery('#undo-publish').hide();	
+				jQuery('#ajax-loading').hide();	
+				jQuery('#save-post').removeClass('button-disabled');
+				jQuery('#publish-alt').removeClass('button-primary-disabled');
+					
+			}
+			
+		</script>
+	<?php
+	
+	endif;
+}
